d2c86ce55a6388d72eab29c9a0cbbeaebdb8ac65
Jemil Castro <jemilcastro@MacBook-Air-de-Jemil-3.local>
2024-10-30 23:55:18 -0500

    Commit inicial

---
+++ 01-variables.py
@@ -0,0 +1,11 @@
+" VARIABLES"
+
+nombre1 = "Hola"
+NOMBRE = " mundo"
+
+print(nombre1, NOMBRE)
+
+nomresito = 2
+carlos = 1.2
+ds = True
+fsd = False
---
+++ 02-strings.py
@@ -0,0 +1,8 @@
+nombre = "Ultimate Python"
+descripcion = """
+Ultimate Python,
+este curso contempla todos los detalles
+que necesitas aprender para encontrar
+un trabajo como programador.
+"""
+print(len(nombre))
